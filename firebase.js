7eb50277dfc2b663d03a86774a02b913b9857dc6
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-08-13 23:55:34 -0400

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEMmk8ePGKWvU36-D1QQKI1r8bV80fqmo",
+  authDomain: "flashcardsaas-d7f55.firebaseapp.com",
+  projectId: "flashcardsaas-d7f55",
+  storageBucket: "flashcardsaas-d7f55.appspot.com",
+  messagingSenderId: "949126139782",
+  appId: "1:949126139782:web:e5d9dbdd3e760ffb3be607",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
